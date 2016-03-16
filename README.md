41e2947fb4c2787c8d1ab0d10c0c0e488ceb7e25
Jiro Iwamoto <jirokun@gmail.com>
2016-03-16 22:59:34 +0900

    セットアップ方法の変更

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 データベースからデータレポートを簡単に出力するためのレポーティングフレームワーク
 
 ## セットアップ
+現状のサンプルを動かすにはPostgreSQLが必要です。
+
 ```bash
+# サンプルデータの作成
+cd sample
+ruby create_data.rb
+
+# サンプルデータの登録
+psql <database> < create_table.sql
+psql <database> < users_data.sql
+psql <database> < transaction_data.sql
+
+# サーバの設定・起動
 npm install
 npm start
 ```
--- sample/create_data.rb
+++ sample/create_data.rb
@@ -1,8 +1,9 @@
 require 'faker'
 Faker::Config.locale = :ja
 
-if false
-puts <<EOT
+puts "making data..."
+open('create_table.sql', 'w') do |f|
+  f.puts <<EOT
 drop table if exists transactions;
 drop table if exists users;
 create table users (
@@ -23,8 +24,8 @@ create table transactions (
 	created_date timestamp
 );
 EOT
-
 end
+
 
 users = "0000000001".upto("0001000000").map {|i| i }
 specialties = 'A'.upto('Z').map do |c|
@@ -37,15 +38,23 @@ empolyments = 100.times.map {
   }
 }
 
-if false
-puts "COPY users (id, sei, mei, age, specialty, employment, employee_number, last_login) FROM stdin;"
-users.each do |user|
-  employment = empolyments[rand(empolyments.size)]
-  puts [user, Faker::Name.last_name, Faker::Name.first_name, rand(60), specialties[rand(specialties.size)], employment[:name], employment[:employee_number], Faker::Time.backward(1000, :evening).strftime('%Y-%m-%d %H:%M:%S')].join("\t")
-end
+open('users_data.sql', 'w') do |f|
+  f.puts "COPY users (id, sei, mei, age, specialty, employment, employee_number, last_login) FROM stdin;"
+  users.each do |user|
+    employment = empolyments[rand(empolyments.size)]
+    f.puts [user, Faker::Name.last_name, Faker::Name.first_name, rand(60), specialties[rand(specialties.size)], employment[:name], employment[:employee_number], Faker::Time.backward(1000, :evening).strftime('%Y-%m-%d %H:%M:%S')].join("\t")
+  end
 end
 
-puts "COPY transactions (id, user_id, amount, created_date) FROM stdin;"
-1.upto(1000000) do |i|
-  puts [i, users[rand(users.size)], rand(10000) + 100, Date.today - rand(1000)].join "\t"
+open('transaction_data.sql', 'w') do |f|
+  f.puts "COPY transactions (id, user_id, amount, created_date) FROM stdin;"
+  1.upto(1000000) do |i|
+    f.puts [i, users[rand(users.size)], rand(10000) + 100, Date.today - rand(1000)].join "\t"
+  end
 end
+
+puts <<EOT
+psql <database> < create_table.sql
+psql <database> < users_data.sql
+psql <database> < transaction_data.sql
+EOT
